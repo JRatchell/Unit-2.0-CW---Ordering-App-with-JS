ed26176495159957da8aacdfb376af76ae87567b
JRatchell <jrboston1@gmail.com>
2023-02-15 05:52:42 -0500

    second commit with the skeleton of functions being added

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-function BobaOrder() {
-let DrinkChoice = prompt ("Which drink would you like to order?");
-
+function DrinkOrder() {
+var DrinkChoice = prompt ("Which drink would you like to order?");
+    document.getElementById("Drink").innerHTML= DrinkChoice ;  
+}
+function DrinkSize() {
+    var SizeChoice = prompt ("What Size would you like your drink to be?")
+    document.getElementById("Size").innerHTML= SizeChoice ;
+}
+function DrinkIceLevel () {
+    var IceLevel = prompt ("About how much ice would you like in your drink?")
+    document.getElementById("Ice").innerHTML = IceLevel ;
+}
+function DrinkSugarLevel () {
+    var SugarLevel = prompt ("How much sugar would you like in your drink?")
+    document.getElementById ("Sugar").innerHTML = SugarLevel ;
+}
+function DrinkTopping () {
+    var Topping = prompt ("What topping would you like for your drink?")
+    document.getElementById ("Topping").innerHTML = Topping ;
 }
