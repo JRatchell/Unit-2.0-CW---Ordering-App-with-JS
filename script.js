72c00946934a7cf0ac7c0ccaebc2aba6e4b2174b
JRatchell <jrboston1@gmail.com>
2023-02-15 10:42:52 -0500

    first version of converging all the functions into a singular one

--- script.js
+++ script.js
@@ -1,22 +1,48 @@
-function DrinkOrder() {
-var DrinkChoice = prompt ("Which drink would you like to order?");
-    document.getElementById("Drink").innerHTML= DrinkChoice ;  
+function drinkOrder() {
+let drinkChoice = prompt ("Which drink would you like to order?");
+    document.getElementById("Drink").innerHTML= drinkChoice ;  
 }
-function DrinkSize() {
-    var SizeChoice = prompt ("What Size would you like your drink to be?")
-    document.getElementById("Size").innerHTML= SizeChoice ;
+function drinkSize() {
+    let sizeChoice = prompt ("What Size would you like your drink to be?")
+    document.getElementById("Size").innerHTML= sizeChoice ;
 }
 function DrinkIceLevel () {
-    var IceLevel = prompt ("About how much ice would you like in your drink?")
-    document.getElementById("Ice").innerHTML = IceLevel ;
+    let iceLevel = prompt ("About how much ice would you like in your drink?")
+    document.getElementById("Ice").innerHTML = iceLevel ;
 }
-function DrinkSugarLevel () {
-    var SugarLevel = prompt ("How much sugar would you like in your drink?")
-    document.getElementById ("Sugar").innerHTML = SugarLevel ;
+function drinkSugarLevel () {
+    let sugarLevel = prompt ("How much sugar would you like in your drink?")
+    document.getElementById ("Sugar").innerHTML = sugarLevel ;
 }
-function DrinkTopping () {
-    var Topping = prompt ("What topping would you like for your drink?")
-    document.getElementById ("Topping").innerHTML = Topping ;
+/*function drinkTopping () {
+    let topping = prompt ("What topping would you like for your drink?")
+    document.getElementById ("Topping").innerHTML = topping ;
+}*/
+
+function whatever() {
+    let topping = prompt ("What topping would you like for your drink?")
+    const topping1= "Mango Star Jelly"
+    const topping2 = "Lychee Jelly"
+    const topping3 = "Boba Original"
+    switch (topping) {
+        case topping1:
+            document.getElementById("Topping").innerHTML = topping ;
+            break;
+
+        case topping2:
+            document.getElementById("Topping").innerHTML = topping ;
+            break;
+
+        case topping3:
+            document.getElementById("Topping").innerHTML = topping ;
+            break;
+
+    
+        default:
+            document.getElementById("debug").innerHTML = "Please enter a valid topping";
+            break;
+    }
 }
+
 
  /* Need to ask Kit how to roll these into a singular function rather than them being a bunch of separate ones, */
